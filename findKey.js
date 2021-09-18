52089ff9fec45a750b596cc64e0777625b54728d
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-18 11:53:34 -0400

    Create function which takes in an object and a call back

---
+++ findKey.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😵‍💫😵‍💫😵‍💫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const objectKey = Object.keys(object);
+  for (let arr of objectKey) {
+    if (callback(object[arr]) === true) {
+      return arr;
+    }
+  }
+};
+
+// TEST CODE
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+, "noma"));
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "noma"
+, "elBulli"));
